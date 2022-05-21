8facb1e7c83257e2d1ee36360e3a9aa1604ede51
Yoann Agésilas <yoann.agesilas@gmail.com>
2022-05-21 13:25:54 +0200

    feat: add car and road

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
-import './style.css'
+import './style.css';
+import { Car } from './Car';
+import { Road } from './Road';
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const canvas = document.getElementById('canvas') as HTMLCanvasElement;
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+canvas.width = 200;
+
+const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+const road = new Road(canvas.width / 2, canvas.width * 0.9);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50);
+
+animate();
+
+function animate() {
+    car.update();
+
+    canvas.height = window.innerHeight;
+
+    ctx.save();
+    ctx.translate(0, -car.y + canvas.height * 0.7);
+
+    road.draw(ctx);
+    car.draw(ctx);
+
+    ctx.restore();
+    requestAnimationFrame(animate);
+}
